62a24d2036f0da78df3726c13a29d1afe29140e1
EC2 Default User <ec2-user@ip-172-31-31-97.ec2.internal>
2020-03-26 16:24:10 +0000

    Initial push.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import CompanySearch from './CompanySearch';
+import rows from './Companies'
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <CompanySearch rows={rows} />
     </div>
   );
 }
 
 export default App;
+
