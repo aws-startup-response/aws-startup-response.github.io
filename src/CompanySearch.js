9df40b3827cff4d1309abb7018606aebc37c1e35
EC2 Default User <ec2-user@ip-172-31-31-97.ec2.internal>
2020-03-29 19:42:59 +0000

    changing logo to be the more consistent non-thumbnail version

--- src/CompanySearch.js
+++ src/CompanySearch.js
@@ -180,7 +180,8 @@ If you believe you have a relevant solution and wish us to evaluate for inclusio
                     var logoUrl = "";
                     
                     if(item["Logo"][0]){
-                      logoUrl = item["Logo"][0]["thumbnails"]["large"]["url"];
+                      //logoUrl = item["Logo"][0]["thumbnails"]["large"]["url"];
+                      logoUrl = item["Logo"][0]["url"];
                     }
 
                     var contactString = "mailto:hcls-startups@amazon.com?subject=Introduction to " + item.Customer +"&body=(Please include your name, company, and title)";
